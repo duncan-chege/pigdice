ea7210c63b8e87012c6e6ba61c27e2b0b94b0ecc
dunyung1 <dshege4@gmail.com>
2018-12-19 20:12:20 +0300

    remove irrelevant commits

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,6 @@ $(document).ready(function() {
     parseInt($(".playerscores").text(currentDice));
 
     if (currentDice != 1) {
-      //parseInt($(".playerfinal").text(0));
-      // return 0;
-
       total += currentDice;
       parseInt($(".playerfinal").text(total));
     } else {
@@ -23,15 +20,3 @@ $(document).ready(function() {
     }
   });
 });
-// for(var i=0; i<6; i++){
-//   d1Array.push(Math.floor(Math.random()*6)+1);
-// }
-//   d1Array.forEach(function(d1Value){
-//     total+=d1Value;
-//     parseInt($(".playerscores").text(d1Value));
-//   });
-//   parseInt($(".playerfinal").text(total));
-// if(d1==1){
-//   $(".playerscores").text("0");
-//   $(".playerfinal").text("0");
-// }
